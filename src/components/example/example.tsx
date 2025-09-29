1ccbf6524ea8dc967fb2bae9e973572b2facb605
chen yang <wszgrcy@gmail.com>
2025-09-29 10:38:35 +0800

    1

--- src/components/example/example.tsx
+++ src/components/example/example.tsx
@@ -1,23 +1,18 @@
-import { $, component$, noSerialize, useSignal, useStore, useTask$, useVisibleTask$ } from '@builder.io/qwik';
+import { $, component$, isServer, noSerialize, useSignal, useStore, useTask$, useVisibleTask$ } from '@builder.io/qwik';
 import { useTest } from './useTest';
 
 export const ExampleTest = component$((props: { flag: boolean }) => {
-  let a = useTest(noSerialize(() => '123456')!);
   useTask$(({ track }) => {
-    track(() => {
-      return a.value;
-    });
-    console.log('data1', a.value);
+
+    console.log('data1', isServer);
   });
   useVisibleTask$(({ track }) => {
-    track(() => {
-      return a.value;
-    });
-    console.log('data2', a.value);
+
+    console.log('data2', isServer);
   });
   return (
     <>
-      <div>{a.value}</div>
+      1
     </>
   );
 });
