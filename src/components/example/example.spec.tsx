24841b55b3352a26870674383ebd516f9f5fb560
chen yang <wszgrcy@gmail.com>
2025-09-29 10:45:53 +0800

    1

--- src/components/example/example.spec.tsx
+++ src/components/example/example.spec.tsx
@@ -3,13 +3,17 @@ import { test, expect } from 'vitest';
 import { ExampleTest } from './example';
 
 test(`test1`, async () => {
-  const { screen, render } = await createDOM();
+  const { screen, render, userEvent } = await createDOM();
   await render(<ExampleTest flag={true} />);
+  await userEvent(screen.querySelector('.change-btn'), 'click');
+ 
   await new Promise<void>((resolve) => {
     setTimeout(() => {
       resolve();
-    }, 200);
+    }, 500);
   });
+
   console.log(screen.outerHTML);
-  expect(screen.outerHTML).toContain('123456');
+  expect(screen.outerHTML).toContain('88882');
+  expect(screen.outerHTML).toContain('88881');
 });
--- src/components/example/example.tsx
+++ src/components/example/example.tsx
@@ -1,18 +1,38 @@
-import { $, component$, isServer, noSerialize, useSignal, useStore, useTask$, useVisibleTask$ } from '@builder.io/qwik';
+import { $, component$, isBrowser, isServer, noSerialize, useSignal, useStore, useTask$, useVisibleTask$ } from '@builder.io/qwik';
 import { useTest } from './useTest';
 
 export const ExampleTest = component$((props: { flag: boolean }) => {
+  let value2 = useSignal('0');
+  let a = useTest(() => value2.value);
   useTask$(({ track }) => {
-
-    console.log('data1', isServer);
+    track(() => {
+      return value2.value;
+    });
+    console.log('data1', value2.value);
+    setTimeout(() => {
+      a.value = value2.value;
+      console.log('aaa',a.value);
+    }, 100);
   });
   useVisibleTask$(({ track }) => {
-
-    console.log('data2', isServer);
+    track(() => {
+      return value2.value;
+    });
+    console.log('data2', value2.value);
   });
   return (
     <>
-      1
+      <button
+        class="change-btn"
+        onClick$={() => {
+          value2.value = '8888';
+          console.log('onClick', value2.value);
+        }}
+      >
+        change value1
+      </button>
+      <div>{a.value}1</div>
+      <div>{value2.value}2</div>
     </>
   );
 });
